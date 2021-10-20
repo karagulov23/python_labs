1dc528eda47448424d86378e5d5a34cb7996c3d1
Sultan <karagulov.sultan@gmail.com>
2021-10-20 13:47:47 +0600

    message

---
+++ Lab #1/2.py
@@ -0,0 +1,16 @@
+
+print("\nОбщество в начале XXI века\n")
+year=int(input("Введите Ваш возраст: "))
+if year>=0 and year<=6:
+    print("\nВам в детский сад!\n")
+elif year>=7 and year<=18:
+    print("\nВам в школу!\n")
+elif year>=19 and year<=25:
+    print("\nВам в профессиональное учебное заведение!\n")
+elif year>=26 and year<=60:
+    print("\nВам на работу!\n")
+elif year>=61 and year<=120:
+    print("\nВам давно пора на пенсию!\n")
+elif year<0 or year>120:
+     for i in range(0,5):
+        print("\nОшибка! Это программа для людей!\n")
